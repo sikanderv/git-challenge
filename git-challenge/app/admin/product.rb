49a308ac4e3ce173aba04f6aa3a9864fbd729107
Sikander <sikanderv@gmail.com>
2017-03-15 00:44:35 +0000

    Make categories and products editable by admin

---
+++ git-challenge/app/admin/product.rb
@@ -0,0 +1,12 @@
+ActiveAdmin.register Product do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+  belongs_to :category
+  permit_params :name, :price, :category_id
+
+end
